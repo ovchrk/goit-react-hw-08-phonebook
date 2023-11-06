f8df83efe9751886f968867a7d9a855dfc19ba7a
ovchrk <113932934+ovchrk@users.noreply.github.com>
2023-11-06 22:01:52 +0200

    persist removed

--- src/index.js
+++ src/index.js
@@ -5,15 +5,15 @@ import './index.css';
 import { Provider } from 'react-redux';
 import { persistor, store } from 'redux/store';
 import { BrowserRouter } from 'react-router-dom';
-import { PersistGate } from 'redux-persist/integration/react';
+// import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/goit-react-hw-08-phonebook">
       <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <App />
-        </PersistGate>
+        {/* <PersistGate loading={null} persistor={persistor}> */}
+        <App />
+        {/* </PersistGate> */}
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
